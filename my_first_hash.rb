67c6418f6787d7444dcd5c1e1c5adea848eb1d04
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 15:18:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,17 +20,17 @@ def shipping_manifest
   the_manifest
 end
 
-# def retrieval
-#   shipping_manifest = {
-#                   "whale bone corsets" => 5, 
-#                   "porcelain vases" => 2, 
-#                   "oil paintings" => 3
-#                 }
+def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                }
 
-#   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-#   shipping_manifest['oil paintings']
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest['oil paintings']
 
-# end
+end
 
 # def adding
 #   shipping_manifest = {
