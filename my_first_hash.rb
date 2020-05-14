785d6641de473a66a64a2c1f9798944330f6243f
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 15:16:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,10 +17,7 @@ def shipping_manifest
   the_manifest["whale bone corsets"] = 5
   the_manifest["porcelain vases"] = 2
   the_manifest["oil painting"] = 3
-  
-  
-
-
+  the_manifest
 end
 
 def retrieval
